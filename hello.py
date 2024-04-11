62b4fa7837df11992095f3ffa220301983a21d75
xialujun <810108500@qq.com>
2024-04-11 15:47:58 +0800

    ss

---
+++ hello.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from markupsafe import escape
+
+# app = Flask(__name__)
+#
+# @app.route('/user/<name>')
+# def user_page(name):
+#     return f'user:{escape(name)}'
+#
+# @app.route('/')
+# def hello():
+#     return 'hello'
+#
+# @app.route('/test')
+# def test_url_for():
+#     print(url_for('hello'))
+#     print(url_for('user_page', name='lili'))
+#     print(url_for('user_page', name='lici'))
+#     print(url_for('test_url_for'))
+#     print(url_for('test_url_for', num=2))
+#     return 'Test page'
+
+
+name = 'lujun'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
