0ea5add76cd88c77d07d5f4b5183cc145cd19367
xialujun <810108500@qq.com>
2024-04-13 21:07:23 +0800

    数据的增删改查

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ import os.path
 import sys
 
 import click
-from flask import Flask, current_app, render_template
+from flask import Flask, current_app, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -66,7 +66,7 @@ with app.app_context():
 # ctx.push()
 # a = current_app
 # ctx.pop()
-
+app.config['SECRET_KEY'] = 'dev'
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -94,11 +94,11 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.')
 
-@app.route('/')
-def index():
-    movies = Movie.query.all()
-    print(len(movies))
-    return render_template('index.html',  movies=movies)
+# @app.route('/')
+# def index():
+#     movies = Movie.query.all()
+#     print(len(movies))
+#     return render_template('index.html',  movies=movies)
 
 @app.cli.command("forge")
 def forge():
@@ -138,6 +138,61 @@ def page_not_found(e):
 def inject_user():
     user = User.query.filter_by(name='lujun').first()
     return dict(user=user)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('invalid input')
+            return redirect(url_for('index'))
+
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('invalid input')
+            return redirect(url_for('edit', movie_id=movie_id))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+    return render_template('edit.html', movie=movie)
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
