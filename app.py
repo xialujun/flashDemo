3adb1515fe88ec4ae30c432c2ba57e213757faa9
xialujun <810108500@qq.com>
2024-04-13 11:49:59 +0800

    魔板优化

--- app.py
+++ app.py
@@ -59,10 +59,21 @@ else:
 
 
 app = Flask(__name__)
+with app.app_context():
+    a = current_app
+    print(a)
+# ctx = app.app_context()
+# ctx.push()
+# a = current_app
+# ctx.pop()
+
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+with app.app_context():
+    a = current_app
+    print(a)
+    db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -85,40 +96,48 @@ def initdb(drop):
 
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template(index.html, user=user, movies=movies)
+    print(len(movies))
+    return render_template('index.html',  movies=movies)
 
-@app.cli.command()
+@app.cli.command("forge")
 def forge():
     """Generate fake data."""
     db.create_all()
 
-name = 'lujun'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+    name = 'lujun'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
 
-user = User(name=name)
-db.session.add(user)
-for m in movies:
-    movie = Movie(title=m['title'], year=m['year'])
-    db.session.add(movie)
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
 
-db.session.commit()
-click.echo('Done.')
+    db.session.commit()
+    click.echo('Done.')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
+
+@app.context_processor
+def inject_user():
+    user = User.query.filter_by(name='lujun').first()
+    return dict(user=user)
 
 
 
